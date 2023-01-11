e5e7c2f8128de9ee80cc18155a15bd3a990bbd13
Renne Rocha <renne@rennerocha.com>
2023-01-11 17:48:18 -0300

    Initial commit

---
+++ 2013/feedback/index.php
@@ -0,0 +1,3 @@
+<?
+header('Location: https://docs.google.com/forms/d/1wA9ETRQEeCzI-_4YRpVIpMm4GXfFe2DjGEi04MrC-iQ/viewform');
+?>
---
+++ 2014/inscricao/index.php
@@ -0,0 +1,3 @@
+<?
+  header("Location: https://docs.google.com/forms/d/1nqkODafV5zKT2pOG6czbOKWjzk-jvzq0T4izv4GjJZU/viewform");
+?>
